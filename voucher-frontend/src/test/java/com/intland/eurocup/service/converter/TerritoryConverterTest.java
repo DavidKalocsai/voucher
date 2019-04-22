47b26c6fcc3840f98ae4a12ae4b4b37223c0b9c8
David Kalocsai <kalocsaidavid@gmail.com>
2019-04-22 23:31:12 +0200

    TerritoryConverter test

---
+++ voucher-frontend/src/test/java/com/intland/eurocup/service/converter/TerritoryConverterTest.java
@@ -0,0 +1,52 @@
+package com.intland.eurocup.service.converter;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.intland.eurocup.common.model.Territory;
+import com.intland.eurocup.service.converter.exception.UnkownTerritoryException;
+
+
+/**
+ * {@link TerritoryConverter}
+ */
+@RunWith(SpringRunner.class)
+public class TerritoryConverterTest {
+
+  @TestConfiguration
+  static class TerritoryConverterTestContext {
+    @Bean
+    public TerritoryConverter territoryConverter() {
+      return new TerritoryConverter();
+    }
+  }
+
+  @Autowired
+  private TerritoryConverter converter;
+  
+  @Test
+  public void setAsTextShouldSetValueWhenTerritoryValid() {
+    //given
+
+    //when
+    converter.setAsText(Territory.GER.getDbCode());
+    
+    //then
+    Assert.assertEquals(Territory.GER, converter.getValue());    
+  }
+  
+  @Test(expected = UnkownTerritoryException.class)
+  public void setAsTextShouldThrowExceptoinWhenTerritoryInValid() {
+    //given
+
+    //when
+    converter.setAsText("This is not a valid Territory");
+    
+    //then
+  }
+}
