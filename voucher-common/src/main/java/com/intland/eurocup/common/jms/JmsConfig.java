e9c479f288359bdc37edd9f290b3f58dd9089fa8
David Kalocsai <kalocsaidavid@gmail.com>
2019-04-23 13:31:08 +0200

    JMS moved to common

---
+++ voucher-common/src/main/java/com/intland/eurocup/common/jms/JmsConfig.java
@@ -0,0 +1,9 @@
+package com.intland.eurocup.common.jms;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:jms-application.properties")
+public class JmsConfig {
+}
